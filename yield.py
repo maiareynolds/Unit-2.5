26a6c0adec5d25b2612f0cc8c1a2ba8dda0e2ec3
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-09 08:47:15 -0500

    Updated from Brython Server: 2/9/2018 8:47:14 AM

--- yield.py
+++ yield.py
@@ -1,1 +1,19 @@
+#Maia Reynolds
+#2/9/18
+#yield.py - displays yield sign
 
+from ggame import *
+yellow=Color(0xFFFF00,1)
+black=Color(0x000000,1)
+yOutline=LineStyle(20,yellow)
+bOutline=LineStyle(6,black)
+
+outer=PolygonAsset([(0,0),(200,0),(100,180)],yOutline,black)
+inner=PolygonAsset([(0,0),(190,0),(95,170)],bOutline,yellow)
+yIeld=TextAsset("YIELD",fill=black,style="bold 30pt Ariel")
+
+Sprite(outer,(390,150))
+Sprite(inner,(410,170))
+Sprite(yIeld,(445,190))
+
+App().run()
