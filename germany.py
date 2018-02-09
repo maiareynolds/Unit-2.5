dfa73465bb118f9a137306c39b7d828b68dd5fc5
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-08 19:12:57 -0500

    Updated from Brython Server: 2/8/2018 7:12:57 PM

--- germany.py
+++ germany.py
@@ -5,8 +5,12 @@
 from ggame import *
 blackcolor=Color(0x000000,1)
 blackoutline=LineStyle(1,blackcolor)
+redcolor=Color(0xFF0000,1)
+redoutline=LineStyle(1,redcolor)
 
 black=RectangleAsset(1100,150,blackoutline,blackcolor)
+red=RectangleAsset(1100,150,redoutline,redcolor)
 
 Sprite(black)
+Sprite(red,(0,150))
 App().run()
