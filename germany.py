e0fc0e6b2e723ac24ff9923fddf10b53844adf6b
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-08 19:11:32 -0500

    Updated from Brython Server: 2/8/2018 7:11:32 PM

--- germany.py
+++ germany.py
@@ -1,1 +1,12 @@
+#Maia Reynolds
+#2/8/18
+#germany.py - german flag
 
+from ggame import *
+blackcolor=Color(0x000000,1)
+blackoutline=LineStyle(1,blackcolor)
+
+black=RectangleAsset(1100,150,blackoutline,blackcolor)
+
+Sprite(black)
+App().run()
