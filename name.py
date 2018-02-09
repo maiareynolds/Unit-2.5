81f0347b690eb57e28bdeaf0dcdc9abb366b9a18
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-02-09 08:34:56 -0500

    Updated from Brython Server: 2/9/2018 8:34:55 AM

--- name.py
+++ name.py
@@ -1,1 +1,24 @@
+#Maia Reynolds
+#2/8/18
+#name.py - name and color
 
+from ggame import *
+entername=input("Enter your name: ")
+colorchoice=input("Enter RGB Color Code: ")
+
+backcolor=Color("0x"+colorchoice,1)
+outline=LineStyle(1,backcolor)
+black=Color(0x000000,1)
+white=Color(0xFFFFFF,1)
+
+back=RectangleAsset(1100,700,outline,backcolor)
+
+if colorchoice=="000000":
+    name=TextAsset(entername,fill=white,style="bold 60pt Times")
+else:
+    name=TextAsset(entername,fill=black,style="bold 60pt Times")
+
+Sprite(back)
+Sprite(name,(425,200))
+
+App().run()
