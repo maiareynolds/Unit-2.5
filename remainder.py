720bfd1500e0dfc9cd49d3c5a2f94a56709935fc
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-07 08:39:15 -0500

    Updated from Brython Server: 3/7/2018 8:39:15 AM

--- remainder.py
+++ remainder.py
@@ -6,14 +6,14 @@ from random import randint
 from ggame import *
 
 yellow=Color(0xFFFF00,1)
-black=Color(0xFFFFFF,1)
+black=Color(0x0000FF,1)
 one=randint(1,100)
 two=randint(1,one)
 ans=int(input("Find the remainder of "+str(one)+"/"+str(two)+": "))
 if ans==one%two:
     happy=CircleAsset(200,LineStyle(1,yellow),yellow)
     smile1=EllipseAsset(500,10,LineStyle(1,black),black)
-    Sprite(smile1)
+    Sprite(smile1,(50,50))
     Sprite(happy,(300,70))
 else:
     print("yay")
