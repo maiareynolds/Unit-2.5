e0c22cf88b7e1feb2521d7df51b23c71cbe20b03
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-07 08:19:16 -0500

    Updated from Brython Server: 3/7/2018 8:19:15 AM

--- remainder.py
+++ remainder.py
@@ -1,1 +1,14 @@
+#Maia Reynolds
+#3/7/18
+#remainder.py - remainder and faces
 
+from random import randint
+from ggame import *
+
+one=randint(1,100)
+two=randint(1,100)
+ans=int(input("Find the remainder of "+one+"/"+two+": "))
+if ans==one/two:
+    
+
+App().run
