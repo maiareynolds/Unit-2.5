28fe423d4653726ac1394f7714d13ec6dba4c933
maiareynolds <35492770+maiareynolds@users.noreply.github.com>
2018-03-07 08:24:12 -0500

    Updated from Brython Server: 3/7/2018 8:24:12 AM

--- remainder.py
+++ remainder.py
@@ -5,10 +5,15 @@
 from random import randint
 from ggame import *
 
+yellow=Color(0xFFFF00,1)
+"""
 one=randint(1,100)
 two=randint(1,100)
 ans=int(input("Find the remainder of "+one+"/"+two+": "))
 if ans==one/two:
-    
+"""
+happy=CircleAsset(60,LineStyle(1,yellow),yellow)
+
+Sprite(happy)
 
 App().run
